e4ce3d8be816deed1c51fbac7c0c7dc11db4872c
zhaoxh <zhaoxh@k365.cn>
2020-09-22 16:17:35 +0800

    计算器

--- app/home/controller/Index.php
+++ app/home/controller/Index.php
@@ -4,37 +4,31 @@
 namespace app\home\controller;
 
 use app\calculator\Operator;
+use app\validate\Calculation;
+use think\exception\ValidateException;
 use think\Validate;
 
 class Index
 {
-    public function index(){
+    public function calculation(){
         $param = input();
         $num_one = $param['num_one'] = 1;
         $num_two = $param['num_two'] = 2;
-        $operator = $param['operator'] = 1;
+        $operator = $param['operator'] = 4;
 
-        $rules = [
-            'num_one'=>'request|number',
-            'num_two'=>'request|number',
-            'operator'=>'request|number'
-        ];
-        $message  =   [
-            'num_one.require' => '数字1必须',
-            'num_one.number' => '数字1必须是数字类型',
-            'num_two.require' => '数字2必须',
-            'num_two.number' => '数字2必须是数字类型',
-            'operator.require' => '计算符号必须',
-            'operator.number' => '计算符号必须是数字类型'
-        ];
-        //todo vaildata 验证必传
-        $validate = Validate();
-        $validate->message($message);
-//        if($validate->check($param)){
-//            dump($validate->getError());
-//        }
+        //vaildata 验证必传
+        try {
+            validate(Calculation::class)->check($param);
+        } catch (ValidateException $e) {
+            // 验证失败 输出错误信息
+            dump($e->getError());die;
+        }
         $operator = new Operator($param);
         $number = $operator->getResult();
         return $number;
+    }
+
+    public function test(){
+        echo '测试';
     }
 }
